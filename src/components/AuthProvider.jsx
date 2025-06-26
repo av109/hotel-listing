369b7da2b1ff962c82a9df12626379c1b4090381
ayushv <rwtsiya025@gmail.com>
2025-06-26 12:11:42 +0530

    Refactor AuthProvider to include request and response interceptors for token management

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from 'react';
 
 import api from '@/api';
 
@@ -17,7 +23,7 @@ export const useAuth = () => {
 const AuthProvider = ({ children }) => {
   const [token, setToken] = useState();
 
-    useEffect(() => {
+  useEffect(() => {
     const fetchMe = async () => {
       try {
         const response = await api.get('/api/me');
@@ -29,6 +35,53 @@ const AuthProvider = ({ children }) => {
 
     fetchMe();
   }, []);
+
+  useLayoutEffect(() => {
+    const authInterceptor = api.interceptors.request.use((config) => {
+      config.headers.Authorization =
+        !config._retry && token
+          ? `Bearer ${token}`
+          : config.headers.Authorization;
+      return config;
+    });
+
+    return () => {
+      api.interceptors.request.eject(authInterceptor);
+    };
+  }, [token]);
+
+  useLayoutEffect(() => {
+    const refreshInterceptor = api.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (
+          error.response.status === 403 &&
+          error.response.data.message === 'Unauthorized'
+        ) {
+          try {
+            const response = await api.get('/api/refreshToken');
+
+            setToken(response.data.accessToken);
+
+            originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            originalRequest._retry = true;
+
+            return api(originalRequest);
+          } catch {
+            setToken(null);
+          }
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      api.interceptors.response.eject(refreshInterceptor);
+    };
+  }, [token]);
 
   return (
     <AuthContext.Provider value={{ token, setToken }}>
