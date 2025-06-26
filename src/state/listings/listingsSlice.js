1a8ac24b9a6d85f45ec01f9aa4baf9f620c692d2
ayushv <rwtsiya025@gmail.com>
2025-06-26 11:32:49 +0530

    added store, created listingSlice, and fetchListing async thunk

---
+++ src/state/listings/listingsSlice.js
@@ -0,0 +1,26 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import api from '@/api';
+
+const initialState = {
+  listings: [],
+  error: null,
+  status: 'idle',
+};
+
+const listingsSlice = createSlice({
+  name: 'listings',
+  initialState,
+  reducers: {},
+});
+
+export const fetchListings = createAsyncThunk(
+  'listings/fetchListings',
+  async (options) => {
+    const response = await api.get('/api/listings', options);
+    return response.data;
+  },
+);
+
+
+export default listingsSlice.reducer;
